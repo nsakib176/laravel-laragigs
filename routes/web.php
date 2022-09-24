17ba0cbbb44cf9efa76eca7d37c007f1ebbebf4c
Nazmus Sakib <oshantosomuddro@gmail.com>
2022-09-24 09:17:44 +0600

    filtering functionality

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
@@ -14,15 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('listings',[
-        'heading' => "Latest Listings",
-        'listings' => Listing::all()
-    ]);
-});
+//all listing
+Route::get('/', [ListingController::class, 'index']);
 
-Route::get('/listings/{listing}', function(Listing $listing){
-    return view('listing',[
-        'listing' => $listing
-    ]);
-});
+//single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
