8c93d464461aa98fed06f22872ef4e7873aec3f0
Nazmus Sakib <oshantosomuddro@gmail.com>
2022-09-24 12:00:22 +0600

    post job form and validation

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -26,5 +27,20 @@ class ListingController extends Controller
     //show create form
     public function create(){
         return view('listings.create');
+    }
+
+    //store listing data
+    public function store(Request $request){
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,13 @@ use Illuminate\Support\Facades\Route;
 //all listing
 Route::get('/', [ListingController::class, 'index']);
 
-//single listing
-Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
 //show create form
 Route::get('/listings/create', [ListingController::class, 'create']);
+
+//store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
+
+
+//single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
