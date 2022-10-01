40c140a250412c9be8b640f4c0c00a59efe862dd
Nazmus Sakib <oshantosomuddro@gmail.com>
2022-10-01 09:51:12 +0600

    linking listings table with user and seeding db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        Listing::factory(12)->create();
+        $user = User::factory()->create([
+            'name' => 'Joe Harrish',
+            'email' => 'joe@gmail.com'
+        ]);
+
+        Listing::factory(12)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
